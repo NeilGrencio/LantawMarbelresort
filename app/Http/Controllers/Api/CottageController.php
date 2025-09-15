42d740169595ea86f19acdfc4bf9a341de82c26c
levijanclavesillas <levijanclavesillas@gmail.com>
2025-09-15 22:38:28 +0800

    updating cottages table

---
+++ app/Http/Controllers/Api/CottageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\CottageTable;
+use Illuminate\Http\Request;
+
+class CottageController extends Controller
+{
+     public function index()
+    {
+        $cottages = CottageTable::where('status', 'available')
+            ->get()
+            ->map(function ($cottage) {
+                $cottage->image = url('uploads/' . $cottage->image);
+                return $cottage;
+            });
+
+        return response()->json($cottages);
+    }
+}
