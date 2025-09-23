693d27f23387aa786c1f406701913e9ad3fc0ac1
levijanclavesillas <levijanclavesillas@gmail.com>
2025-09-23 15:44:05 +0800

    make edit

--- resources/views/receptionist/edit_booking.blade.php
+++ resources/views/receptionist/edit_booking.blade.php
@@ -91,13 +91,14 @@
                     </div>
                     <label>Check-in:
                         <input type="date" name="checkin"
-                            value="{{ old('checkin', $bookingData->checkin->format('Y-m-d')) }}" required>
+                            value="{{ old('checkin', \Carbon\Carbon::parse($bookingData->checkin)->format('Y-m-d')) }}"
+                            required>
                     </label>
                     <label>Check-out:
                         <input type="date" name="checkout"
-                            value="{{ old('checkout', $bookingData->checkout->format('Y-m-d')) }}" required>
+                            value="{{ old('checkout', \Carbon\Carbon::parse($bookingData->checkout)->format('Y-m-d')) }}"
+                            required>
                     </label>
-
                     {{-- Guest Info --}}
                     <div class="label-container">
                         <h2>Guest Information</h2>
