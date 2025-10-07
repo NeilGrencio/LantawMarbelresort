2e5d9f1c43d7c93bccab9fc3a76f5318e9321b93
levijanclavesillas <levijanclavesillas@gmail.com>
2025-10-07 18:47:35 +0800

    added modified fcm token

--- database/migrations/2025_10_07_184538_add_fcm_token_to_users_table.php
+++ database/migrations/2025_10_07_184538_add_fcm_token_to_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('fcm_token')->nullable()->after('email');
+            $table->string('fcm_token')->nullable(); // removed ->after('email')
         });
     }
 
