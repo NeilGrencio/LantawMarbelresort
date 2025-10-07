e9372c4244e8982cb2428ddf8494e9800f5b2136
levijanclavesillas <levijanclavesillas@gmail.com>
2025-10-07 18:42:12 +0800

    skip menu bookings on mioigrate

--- database/migrations/2025_09_22_081405_create_menu_bookings_table.php
+++ database/migrations/2025_09_22_081405_create_menu_bookings_table.php
@@ -7,16 +7,17 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('menu_bookings', function (Blueprint $table) {
-            $table->id(); // Primary key
-            $table->unsignedBigInteger('menu_id');   // FK to menu table
-            $table->unsignedBigInteger('booking_id'); // FK to booking table
-            $table->integer('quantity')->default(1);
-            $table->decimal('price', 10, 2)->default(0); // price snapshot
-            $table->enum('status', ['pending', 'confirmed', 'cancelled'])->default('pending');
-            $table->timestamps();
-
-        });
+        if (!Schema::hasTable('menu_bookings')) {
+            Schema::create('menu_bookings', function (Blueprint $table) {
+                $table->id(); // Primary key
+                $table->unsignedBigInteger('menu_id');   // FK to menu table
+                $table->unsignedBigInteger('booking_id'); // FK to booking table
+                $table->integer('quantity')->default(1);
+                $table->decimal('price', 10, 2)->default(0); // price snapshot
+                $table->enum('status', ['pending', 'confirmed', 'cancelled'])->default('pending');
+                $table->timestamps();
+            });
+        }
     }
 
     public function down(): void
