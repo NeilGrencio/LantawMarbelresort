34457291543c3064b80653ef5c4c2ef2c52bcc97
levijanclavesillas <levijanclavesillas@gmail.com>
2025-10-09 10:15:17 +0800

    added user notifiable

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Notifications\Notifiable;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
 class User extends Authenticatable
 {
+    use Notifiable;
     protected $table = 'users';
     protected $primaryKey = 'userID';
     public $timestamps = false;
